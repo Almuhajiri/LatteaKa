48e821afe65ecd12124bbada3898d0902ebcc00d
Almuhajiri <yunusmutashimbillah@gmail.com>
2020-02-09 11:12:31 +0700

    Updates

---
+++ precache-manifest.d31e1f953e9cd0de8f78e117f5bea9cd.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86fe2a8a9869c66b5ef40bb1cc8f892",
+    "url": "/Almuhajiri/LatteaKa/index.html"
+  },
+  {
+    "revision": "c652dcf71c982b5d691a",
+    "url": "/Almuhajiri/LatteaKa/static/css/2.59496a39.chunk.css"
+  },
+  {
+    "revision": "99cbaae9ddfc4b767f0b",
+    "url": "/Almuhajiri/LatteaKa/static/css/main.f090af36.chunk.css"
+  },
+  {
+    "revision": "c652dcf71c982b5d691a",
+    "url": "/Almuhajiri/LatteaKa/static/js/2.147b31d0.chunk.js"
+  },
+  {
+    "revision": "4d8864c481fc86e012ea6127ad38587f",
+    "url": "/Almuhajiri/LatteaKa/static/js/2.147b31d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99cbaae9ddfc4b767f0b",
+    "url": "/Almuhajiri/LatteaKa/static/js/main.9f986b9f.chunk.js"
+  },
+  {
+    "revision": "3ea442e774c1dcadca74",
+    "url": "/Almuhajiri/LatteaKa/static/js/runtime-main.f449f710.js"
+  },
+  {
+    "revision": "08b5304ccaccf6ec2a2d9bbf996b8ed0",
+    "url": "/Almuhajiri/LatteaKa/static/media/arrow-right.08b5304c.svg"
+  },
+  {
+    "revision": "2ecb4240cd622d2dc440e2a7f270bd0f",
+    "url": "/Almuhajiri/LatteaKa/static/media/background.2ecb4240.png"
+  },
+  {
+    "revision": "71d6bc5679692e73331b1332bf104768",
+    "url": "/Almuhajiri/LatteaKa/static/media/background2.71d6bc56.png"
+  },
+  {
+    "revision": "9a256e522a6a7629a39465b0627bfc62",
+    "url": "/Almuhajiri/LatteaKa/static/media/background2.9a256e52.svg"
+  },
+  {
+    "revision": "84caf1b121d1446fee81e93d4277cb68",
+    "url": "/Almuhajiri/LatteaKa/static/media/backgroundLanding.84caf1b1.png"
+  },
+  {
+    "revision": "a51075ff3d5121f2b97208e741112835",
+    "url": "/Almuhajiri/LatteaKa/static/media/benefit.a51075ff.svg"
+  },
+  {
+    "revision": "ff2de8b3b50658d2db02681fabc43c05",
+    "url": "/Almuhajiri/LatteaKa/static/media/contact.ff2de8b3.png"
+  },
+  {
+    "revision": "b18228d9c172aafcc0e572b9d44de781",
+    "url": "/Almuhajiri/LatteaKa/static/media/facebook.b18228d9.svg"
+  },
+  {
+    "revision": "d9a683e4d2cfdfebce5295b71218c00a",
+    "url": "/Almuhajiri/LatteaKa/static/media/gallery1.d9a683e4.png"
+  },
+  {
+    "revision": "b0295266c37ef5a4cb06757c9806f4b5",
+    "url": "/Almuhajiri/LatteaKa/static/media/gallery2.b0295266.png"
+  },
+  {
+    "revision": "440d945daba200fe5339721b8a6b3f9e",
+    "url": "/Almuhajiri/LatteaKa/static/media/gallery3.440d945d.png"
+  },
+  {
+    "revision": "4c6c802ea7bfd295b789b0c325631a1b",
+    "url": "/Almuhajiri/LatteaKa/static/media/gallery4.4c6c802e.png"
+  },
+  {
+    "revision": "b50e13949b3634274a33d44f71bd47aa",
+    "url": "/Almuhajiri/LatteaKa/static/media/gallery5.b50e1394.png"
+  },
+  {
+    "revision": "b04a9bdb493c9fad5ec55b58364862f9",
+    "url": "/Almuhajiri/LatteaKa/static/media/gallery6.b04a9bdb.png"
+  },
+  {
+    "revision": "e5c89247bdf63b58cc11ec8f67fb0a59",
+    "url": "/Almuhajiri/LatteaKa/static/media/gallery7.e5c89247.png"
+  },
+  {
+    "revision": "a642263225f0d259c5b1a19715e1e4a9",
+    "url": "/Almuhajiri/LatteaKa/static/media/gallery8.a6422632.png"
+  },
+  {
+    "revision": "f7fc96b609a8fd46fb26d825f0d7f7f7",
+    "url": "/Almuhajiri/LatteaKa/static/media/instagram.f7fc96b6.svg"
+  },
+  {
+    "revision": "894d60f3153f04c3e73a14b41af9f853",
+    "url": "/Almuhajiri/LatteaKa/static/media/menu1.894d60f3.png"
+  },
+  {
+    "revision": "628ab1bb70918dd254d7cadb6b3b7eb1",
+    "url": "/Almuhajiri/LatteaKa/static/media/menu2.628ab1bb.png"
+  },
+  {
+    "revision": "4915d80de662c044e15a61198e147ed5",
+    "url": "/Almuhajiri/LatteaKa/static/media/menu3.4915d80d.png"
+  },
+  {
+    "revision": "5d2f42f78682ccd6c38901d6c2c71bf9",
+    "url": "/Almuhajiri/LatteaKa/static/media/menu4.5d2f42f7.png"
+  },
+  {
+    "revision": "72a2661f32b68d0eae83268fc461e74d",
+    "url": "/Almuhajiri/LatteaKa/static/media/menu5.72a2661f.png"
+  },
+  {
+    "revision": "e7c47facb04692c7034b5c9803653845",
+    "url": "/Almuhajiri/LatteaKa/static/media/menu6.e7c47fac.png"
+  },
+  {
+    "revision": "81178e691cd2d9befc53d3a7e29401a4",
+    "url": "/Almuhajiri/LatteaKa/static/media/mobile-benefit.81178e69.svg"
+  },
+  {
+    "revision": "90a5672570689ec8caf1a50a1496e0e6",
+    "url": "/Almuhajiri/LatteaKa/static/media/mobile-modal.90a56725.svg"
+  },
+  {
+    "revision": "3494357a1dc6b01c317451f9e032f34e",
+    "url": "/Almuhajiri/LatteaKa/static/media/mobile-price.3494357a.svg"
+  },
+  {
+    "revision": "fb43b5279ac61ef4d0d6915946ee9e31",
+    "url": "/Almuhajiri/LatteaKa/static/media/mobile-stepbystep.fb43b527.svg"
+  },
+  {
+    "revision": "66ba6f4e600d15830ed66c47dd292cc2",
+    "url": "/Almuhajiri/LatteaKa/static/media/modal.66ba6f4e.svg"
+  },
+  {
+    "revision": "ac14134510d489d28c729ed68a054f5c",
+    "url": "/Almuhajiri/LatteaKa/static/media/poster.ac141345.png"
+  },
+  {
+    "revision": "cbd21ab1fd015b7fd693cb902337141f",
+    "url": "/Almuhajiri/LatteaKa/static/media/posterFranchise.cbd21ab1.png"
+  },
+  {
+    "revision": "14da3405e365d0070e42bbcce8dfeead",
+    "url": "/Almuhajiri/LatteaKa/static/media/price.14da3405.svg"
+  },
+  {
+    "revision": "573eec52e9c62221ebad20d6ad27d84f",
+    "url": "/Almuhajiri/LatteaKa/static/media/stepbystep.573eec52.svg"
+  },
+  {
+    "revision": "427df52cf9787a6deb5e74967a01655a",
+    "url": "/Almuhajiri/LatteaKa/static/media/toggler-menu.427df52c.svg"
+  },
+  {
+    "revision": "a608385c923019b802c875028eacad4a",
+    "url": "/Almuhajiri/LatteaKa/static/media/twitter.a608385c.svg"
+  }
+]);
